64eeccf039451752ce269f2f8d77e4f9d28fcfdc
planyist <planyist@gmail.com>
2024-02-25 16:11:07 +0900

    feat(information hiding): add information hiding classes and test

---
+++ src/main/java/part3/PersonInfoHideTest.java
@@ -0,0 +1,14 @@
+package part3;
+
+import model.PersonVO;
+
+public class PersonInfoHideTest {
+    public static void main(String[] args) {
+        PersonVO vo = new PersonVO();
+        // vo.name = "홍길동";
+        // vo.age = 50;
+        // vo.phone = "010-1111-1111";
+
+        // System.out.println(vo.name + "\t" + vo.age + "\t" + vo.phone);
+    }
+}
