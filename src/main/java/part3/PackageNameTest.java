c41b17cda780e3980010f96cbd4f76e74a61a8e5
planyist <planyist@gmail.com>
2024-02-25 13:34:44 +0900

    feat(access classes): add access classes and test

---
+++ src/main/java/part3/PackageNameTest.java
@@ -0,0 +1,19 @@
+package part3;
+
+// import java.lang.*;
+
+import model.CarDAO;
+import model.CarDTO;
+
+import java.util.Scanner;
+
+public class PackageNameTest {
+    public static void main(String[] args) {
+        String str = "홍길동";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello World");
+
+        CarDTO carDTO = new CarDTO();
+        CarDAO carDAO = new CarDAO();
+    }
+}
