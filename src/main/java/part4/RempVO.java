8532abd4c446bd6529c2b229388280c6415b8380
planyist <planyist@gmail.com>
2024-02-28 22:17:17 +0900

    feat(inheritance bad case): add inheritance bad case classes and test

--- src/main/java/part4/RempVO.java
+++ src/main/java/part4/RempVO.java
@@ -5,4 +5,13 @@ public class RempVO extends Employee {
         super();
     }
 
+    public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.empDate = empDate;
+        this.dept = dept;
+        this.marriage = marriage;
+    }
+
 }
