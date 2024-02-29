4acb2eb17894f32db1f1bed931f7c11c20b25316
planyist <planyist@gmail.com>
2024-03-01 00:58:20 +0900

    feat(downcasting): add downcasting classes and test

---
+++ src/main/java/part4/ObjectCasting.java
@@ -0,0 +1,16 @@
+package part4;
+
+import model.Animal;
+import model.Cat;
+import model.Dog;
+
+public class ObjectCasting {
+    public static void main(String[] args) {
+        Animal ani = new Dog();
+        ani.eat();
+
+        ani = new Cat();
+        ani.eat();
+        ((Cat) ani).night();
+    }
+}
