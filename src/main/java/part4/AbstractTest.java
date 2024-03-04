f2980f80867f1e884e5cadbc9b5ac636e2b8009e
planyist <planyist@gmail.com>
2024-03-04 23:37:15 +0900

    feat(implemented method in abstract): add implemented method in abstract class(es) and test

--- src/main/java/part4/AbstractTest.java
+++ src/main/java/part4/AbstractTest.java
@@ -6,10 +6,14 @@ import poly.Dog;
 
 public class AbstractTest {
     public static void main(String[] args) {
+        // Animal ani = new Animal();
         Animal ani = new Dog();
         ani.eat();
+        ani.move();
 
         ani = new Cat();
         ani.eat();
+        ani.move();
+        ((Cat) ani).night();
     }
 }
--- src/main/java/poly/Animal.java
+++ src/main/java/poly/Animal.java
@@ -2,4 +2,8 @@ package poly;
 
 public abstract class Animal {
     public abstract void eat();
+
+    public void move() {
+        System.out.println("이동하다.");
+    }
 }
