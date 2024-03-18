e34b761057a6967222e70402fcb387f4ced2ef15
planyist <planyist@gmail.com>
2024-03-18 23:27:03 +0900

    feat(java-made): add java-made class(es) and test

---
+++ src/main/java/part5/RandomAPI.java
@@ -0,0 +1,28 @@
+package part5;
+
+import java.util.Random;
+
+public class RandomAPI {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int[] arr = new int[6];
+        int i = 0;
+        while (i < 6) {
+            int num = rand.nextInt(45) + 1;
+            boolean isDuplicate = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] == num) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                arr[i++] = num;
+            }
+        }
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+    }
+}
