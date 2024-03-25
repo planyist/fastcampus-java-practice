b95b2926c8e3867e09344dbadf5c3d06aaf4ee3b
planyist <planyist@gmail.com>
2024-03-26 00:32:38 +0900

    feat(basic int array): add basic int array class(es) and test

---
+++ src/main/java/part5/IntArrayBasic.java
@@ -0,0 +1,17 @@
+package part5;
+
+public class IntArrayBasic {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        for (int data : nums) {
+            System.out.println(data);
+        }
+
+    }
+}
