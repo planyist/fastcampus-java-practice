b125f53571b56b9cff51c046aeb200638adb4c9c
planyist <planyist@gmail.com>
2024-04-10 13:49:18 +0900

    feat(literal string): add literal string class(es) and test

---
+++ src/main/java/part5/HelloWorldString.java
@@ -0,0 +1,11 @@
+package part5;
+
+public class HelloWorldString {
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println(str);
+
+        String str1 = "HelloWorld";
+        System.out.println(str1);
+    }
+}
