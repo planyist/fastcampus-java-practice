6d39abe90084276b42a5dbf0777ce5bfe23a55c8
planyist <planyist@gmail.com>
2024-04-10 21:53:45 +0900

    feat(string comparison): add string comparison class(es) and test

---
+++ src/main/java/part5/StringCompare.java
@@ -0,0 +1,24 @@
+package part5;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        if (str1.equals(str2)) {
+            System.out.println("str1 == str2");
+        } else {
+            System.out.println("str1 != str2");
+        }
+
+        String str3 = "apple";
+        String str4 = "banana";
+        int result = str3.compareTo(str4);
+        if (result == 0) {
+            System.out.println("str3 == str4");
+        } else if (result < 0) {
+            System.out.println("str3 < str4");
+        } else {
+            System.out.println("str3 > str4");
+        }
+    }
+}
