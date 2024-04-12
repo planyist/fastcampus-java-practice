5f74f113a5b2b34177e54a019464a0cfe9bbe6d8
planyist <planyist@gmail.com>
2024-04-13 01:45:37 +0900

    feat(convert integer to string vice versa): add convert integer to string class(es) and test vice versa

---
+++ src/main/java/part5/IntegerStringTest.java
@@ -0,0 +1,18 @@
+package part5;
+
+public class IntegerStringTest {
+    public static void main(String[] args) {
+        String str1 = "123";
+        String str2 = "123";
+        System.out.println(str1 + str2);
+
+        int num = Integer.parseInt(str1) + Integer.parseInt(str2);
+        System.out.println(num);
+
+        int num1 = 123;
+        int num2 = 123;
+        String str = String.valueOf(num1) + num2;
+        System.out.println(str);
+        System.out.println("" + num1 + num2);
+    }
+}
