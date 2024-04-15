9964045f08ea78762302b5c6d2a2dad29fbc5c3c
planyist <planyist@gmail.com>
2024-04-15 23:06:48 +0900

    feat(restriction on generic): add restriction on generic class(es) and test

---
+++ src/main/java/model/AverageCalculator.java
@@ -0,0 +1,17 @@
+package model;
+
+public class AverageCalculator<T extends Number> {
+    private final T[] numbers;
+
+    public AverageCalculator(T[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public double calculateAverage() {
+        double sum = 0.0;
+        for (T number : numbers) {
+            sum += number.doubleValue();
+        }
+        return sum / numbers.length;
+    }
+}
