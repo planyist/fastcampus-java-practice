78715d113533dd9d661fd70d33e9d9373963bc28
planyist <planyist@gmail.com>
2024-04-16 16:01:40 +0900

    feat(weather openapi example): add weather openapi example class(es) and test

---
+++ src/main/java/part7/WeatherExample.java
@@ -0,0 +1,48 @@
+package part7;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherExample {
+    public static void main(String[] args) {
+        String apiKey = ""; // key from site
+        String city = "Seoul";
+        String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city +
+                "&appid=" + apiKey + "&units=metric";
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer content = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                System.out.println("content.toString() = " + content);
+
+                JsonObject weatherData = JsonParser.parseString(content.toString()).getAsJsonObject();
+                JsonObject mainData = weatherData.getAsJsonObject("main");
+                double temp = mainData.get("temp").getAsDouble();
+                System.out.println("temp = " + temp);
+                connection.disconnect();
+            } else {
+                System.out.println(connection.getResponseCode());
+                System.out.println(connection.getResponseMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
